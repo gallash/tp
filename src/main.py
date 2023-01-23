a65b2d26a32cb76a83453094ace91fdc38ff55e6
Phillip Gallas <gallas.works@outlook.com>
2023-01-22 22:03:15 -0300

    'main' now creates 'alias.yaml', updated 'tests' and 'venv'

--- src/main.py
+++ src/main.py
@@ -3,12 +3,13 @@ import sys
 import yaml
 
 
-ALIAS = "../alias.yaml"
+ALIAS = "alias.yaml"
 
 def read_alias():
     args_list = sys.argv[1:]  # Not including the `main.py` itself
     if os.path.isfile(ALIAS) is False:
-        os.execute("cd .. && touch alias.yaml")
+        os.system(f"touch {ALIAS}")
+    assert os.path.isfile(ALIAS)
 
 if __name__ == '__main__':
     pass
