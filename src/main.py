fbc6c48b8e4fdb4cb3a555dc3d1f214f409cde53
Phillip Gallas <gallas.works@outlook.com>
2023-01-22 21:43:16 -0300

    Created venv environment and structure the first test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 import os
+import sys
 import yaml
 
+
+ALIAS = "../alias.yaml"
+
+def read_alias():
+    args_list = sys.argv[1:]  # Not including the `main.py` itself
+    if os.path.isfile(ALIAS) is False:
+        os.execute("cd .. && touch alias.yaml")
 
 if __name__ == '__main__':
     pass
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,22 @@
 """
 Following the TDD principles, this is the testing script for src/main.py
 """
+import os
+import pytest
+
+from src import main
+
+ 
+ALIAS = "../alias.yaml"
 
 class TestMain_SaveCurrentDirectory:
+    # @pytest.mark.skip(reason="This already works, and will not suffer changes later on")
+    def test_creates_alias_file_when_there_is_none(self):
+        # If there is no 'alias.yaml', check if main.py creates one
+        if os.path.isfile(ALIAS) is False:
+            main.read_alias()
+        assert os.path.isfile(ALIAS)
+
     def test_save_current_dir_with_dot(self):
+        
         pass
