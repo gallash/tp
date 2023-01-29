c1f5882d4313f65a7ce4713d18df0830b5357745
Phillip Gallas <gallas.works@outlook.com>
2023-01-29 18:13:21 -0300

    Saving system works, but has bugs concerning the treatment of blank spaces and underscores

--- src/main.py
+++ src/main.py
@@ -1,30 +1,168 @@
 import os
 import sys
 import yaml
+import types
 
-# Better split this file. This one becomes io.py and the other becomes utils.py
+# Better split this file. This one stays as main.py and the other becomes 
+# utils.py
 
 
 ALIASES = "aliases.yaml"
 
-def read_aliases():
+
+def read_aliases_file() -> dict:
     """
-    Reads the aliases.yaml file and 
+    Reads aliases file. A function for this will help me if I decide to change
+    the extention to TOML
+
+    :returns aliases: The dictionary containing all data stored in the file
     """
-    args_list = sys.argv[1:]  # Not including the `main.py` itself
+    with open(ALIASES) as file:
+        aliases = yaml.load(file, Loader=yaml.FullLoader)
+
+    return aliases
+
+
+def create_default_aliases_file() -> None:
+    """
+    Creates an empty aliases file (with some basic configurations)
+    """
+    with open(ALIASES, 'w') as file:
+        init_config = dict()
+        init_config['BaseList'] = ['default']
+        init_config['Base'] = init_config['BaseList'][0]  # = default
+        
+        init_config['default'] = {}  # 'default' base is created empty
+
+        yaml.dump(init_config, file)
+
+
+def does_dir_path_exist():
+    """ 
+    True -> dir_path exists
+    False -> it does not exist 
+    """
+    pass
+
+
+def get_alias(alias: str):
+    """
+    Searches for the given alias in the base
+    
+    :params alias: String of the name of the alias for the directory
+    :returns dir_path | False: String of the location, if found, or False if no
+    alias was found
+    """
+    # Read aliases of the default base from the aliases file
+    file = read_aliases_file()
+    aliases = list(file[file['Base']].keys())
+
+    if alias in aliases:
+        return aliases[alias]
+    return False
+
+
+def check_whether_aliases_file_exists() -> dict:
+    """
+    Checks whether aliases file exists. If it does not, the function creates 
+    one and returns it.
+    
+    :returns aliases: a dictionary containing all data stored in aliases.
+    """
     if os.path.isfile(ALIASES) is False:
         os.system(f"touch {ALIASES}")
     
-    assert os.path.isfile(ALIASES)
-    with open(ALIASES) as file:
-        stored_aliases = file 
-    return stored_aliases    
-
-
-def save_current_dir(alias: str) -> None:
+    if isinstance(read_aliases_file(), types.NoneType):
+        create_default_aliases_file()
     
+    assert os.path.isfile(ALIASES)
+    aliases = read_aliases_file()
+    return aliases 
 
-def tp()
+
+def check_given_alias(alias: str = None) -> str:
+    """
+    If 'alias' is given and is in the correct format -> returns the corrected
+    format if necessary
+    """
+    if alias is None:
+        alias = input("Give the alias a name (alphanumeric):\t")
+
+    # Checking for incorrect formatting
+    loop = True
+    while(loop):
+        if alias == "" or not alias.isalnum():
+            print("Please name it with letters and/or digits")
+            continue
+        
+        #try:
+        #    assert alias != ""
+        #except:
+        #    raise AliasCannotBeAnEmptyString("Please name it with letters 
+        #            and/or numbers")
+
+        #try:
+        #    assert alias.isalnum()
+        #except:
+        #    print(AliasCannotHaveSpecialCharacters("Please name it with
+        #            letters and/or digits"))
+
+        # Removing blank spaces and separating strings with an underscore
+        try:
+            alias = alias.lstrip().rstrip().replace(' ', '_')  # Does nothing 
+            # if it is already formatted
+
+            # Checks whether alias is already saved in the Base
+            if get_alias(alias):
+                raise Exception(f"{alias} is already in use. Choose another name")
+        except Exception as e:
+            print(e)
+            alias = input("Alias (alphanumeric):\t")
+        else:
+            loop = False
+
+    # If 'alias' is not given -> ask for one
+    # If 'alias' is not in the correct format -> try to correct it. 
+    # If not possible to put it in the correct format ->  ask the user for 
+    #  another name
+    # If the given alias is already registered in the base -> ask the user if 
+    #  he/she wishes to repurpose this alias (give it a new directory to point
+    #  to), or if he/she wishes to choose another name
+    return alias
+
+
+def save_alias(dir_path: str, alias: str = None) -> None:
+    """
+    Save alias will perform checks to see whether this connection can be
+    saved, and then it will save it, should it be allowed. Otherwise, 
+    it will return to the user what went awry
+    """
+    # Checks existance of the target directory, and shape it into the correct 
+    # format
+    if dir_path == '.':
+        dir_path = os.getcwd()  # Check if this function returns the 
+        # directory where the user is, or if it returns the directory where tp
+        # is installed
+    else:
+        assert does_dir_path_exist(dir_path), "The directory could not be found"
+    
+    # Checks if an alias was given. If none was given, prompt the user for one
+    alias = check_given_alias(alias)
+
+    # If all checks are OK, save the alias in the file
+    file = read_aliases_file()
+    with open(ALIASES, 'w') as f:
+        try:
+            file[file['Base']][alias] = dir_path
+            yaml.dump(file, f)
+        except Exception as e:
+            print(e)
+        else:
+            print(f"'{alias}' which points to {dir_path} was saved successfully")
+
+
+def tp(absolute_path: str) -> None:
+    pass
 
 
 if __name__ == '__main__':
--- tp.py
+++ tp.py
@@ -3,12 +3,33 @@ tp - teleport
 Fast travel through your system
 
 Usage:
-tp project: Sends the user to the directory pointed by 'project'
+    `tp` sends the user to the directory pointed by 'project'
 
-Options
+Options:
+    The options the user has are related to creating an alias for a directory, manage the saved aliases and creating and managing the bases (namespaces for aliases).
+
+Alias: passing the name of the alias will tp the user there. E.g., for an alias named 'project', `tp project` will send the user to the project's directory
+
 -s, --save: Save the alias under an identifiable name. Requires a location ('.' is a valid option as the location). Name at this moment is optional (will be asked later if not passed here)
+
+
+-b, --base: Just like namespaces in C++, or workspace in Linux, it separates contexts. So two same-named aliases can coexist, but in different bases. This option will change from the current base to chosen one (changes DEFAULT_BASE in the YAML file). The system starts with a default one
+
+-B, --base-create: creates a new base. Requires a name. Can include a short description
+
+--base-rename: renames currently selected base
+
+--base-list: lists all bases and their descriptions (if any)
+
+--base-delete: deletes given base (and all their aliases)
+
+--base-delete-all: deletes all bases
+
+
 -l, --list: Lists all saved aliases
+
 -d, --delete: Deletes an alias and its location
+
 -D, --Delete: Deletes all aliases and their location
 """
 
@@ -16,9 +37,12 @@ import sys
 import subprocess
 import argparse
 
-from src import io
-from src import utils
+from src import main
+# from src import utils  # Not necessary for the time being
 from src import exceptions
+
+
+ROOT_DIR = ''  # Absolute path to this tp.py
 
 
 def parsing():
@@ -30,18 +54,35 @@ def parsing():
             type=str,
             help="Save a location. Requires a location ('.' is a valid option)."
     )
+    parser.add_argument(
+            '--alias',
+            type=str,
+            help="Alias to be given to a directory."
+    )
+
+    return parser.parse_args()
 
 
-def tp():
-    pass
+def actions_manager(args):
+    """
+    Manages the options' functionalities
+    """
+    if args.save is not None:
+        main.save_alias(args.save, args.alias)
+
+    # In the future, add --list, --delete, --base, etc
+
 
 if __name__ == '__main__':
+    main.check_whether_aliases_file_exists()
+
     if len(sys.argv) == 2:
-        # This function imports io.read_aliases, checks whether the given alias is there, returns the location and we tp the user there
         try:
-            utils.get_location(sys.argv[1])  # Can raise "LocationNotFound"
+            absolute_path = main.get_alias(sys.argv[1])  # Can raise "LocationNotFound"
         except Exception as e:
             print(e)
-        tp()
+        else:
+            main.tp(absolute_path)
     else:
         args = parsing()
+        actions_manager(args)
