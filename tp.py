7646507a3b2a4cf897480e787689387578738d93
Phillip Gallas <gallas.works@outlook.com>
2023-01-30 14:13:16 -0300

    Revert "Saving system works, but has bugs concerning the treatment of blank spaces and underscores"
    This reverts commit c1f5882d4313f65a7ce4713d18df0830b5357745.

--- tp.py
+++ tp.py
@@ -3,33 +3,12 @@ tp - teleport
 Fast travel through your system
 
 Usage:
-    `tp` sends the user to the directory pointed by 'project'
+tp project: Sends the user to the directory pointed by 'project'
 
-Options:
-    The options the user has are related to creating an alias for a directory, manage the saved aliases and creating and managing the bases (namespaces for aliases).
-
-Alias: passing the name of the alias will tp the user there. E.g., for an alias named 'project', `tp project` will send the user to the project's directory
-
+Options
 -s, --save: Save the alias under an identifiable name. Requires a location ('.' is a valid option as the location). Name at this moment is optional (will be asked later if not passed here)
-
-
--b, --base: Just like namespaces in C++, or workspace in Linux, it separates contexts. So two same-named aliases can coexist, but in different bases. This option will change from the current base to chosen one (changes DEFAULT_BASE in the YAML file). The system starts with a default one
-
--B, --base-create: creates a new base. Requires a name. Can include a short description
-
---base-rename: renames currently selected base
-
---base-list: lists all bases and their descriptions (if any)
-
---base-delete: deletes given base (and all their aliases)
-
---base-delete-all: deletes all bases
-
-
 -l, --list: Lists all saved aliases
-
 -d, --delete: Deletes an alias and its location
-
 -D, --Delete: Deletes all aliases and their location
 """
 
@@ -37,12 +16,9 @@ import sys
 import subprocess
 import argparse
 
-from src import main
-# from src import utils  # Not necessary for the time being
+from src import io
+from src import utils
 from src import exceptions
-
-
-ROOT_DIR = ''  # Absolute path to this tp.py
 
 
 def parsing():
@@ -54,35 +30,18 @@ def parsing():
             type=str,
             help="Save a location. Requires a location ('.' is a valid option)."
     )
-    parser.add_argument(
-            '--alias',
-            type=str,
-            help="Alias to be given to a directory."
-    )
-
-    return parser.parse_args()
 
 
-def actions_manager(args):
-    """
-    Manages the options' functionalities
-    """
-    if args.save is not None:
-        main.save_alias(args.save, args.alias)
-
-    # In the future, add --list, --delete, --base, etc
-
+def tp():
+    pass
 
 if __name__ == '__main__':
-    main.check_whether_aliases_file_exists()
-
     if len(sys.argv) == 2:
+        # This function imports io.read_aliases, checks whether the given alias is there, returns the location and we tp the user there
         try:
-            absolute_path = main.get_alias(sys.argv[1])  # Can raise "LocationNotFound"
+            utils.get_location(sys.argv[1])  # Can raise "LocationNotFound"
         except Exception as e:
             print(e)
-        else:
-            main.tp(absolute_path)
+        tp()
     else:
         args = parsing()
-        actions_manager(args)
